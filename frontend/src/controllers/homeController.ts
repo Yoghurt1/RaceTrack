8d1a5863661c306eee376971fc824aa623d85f9f
Yoghurt1 <16927289+Yoghurt1@users.noreply.github.com>
2022-05-21 12:56:56 +0100

    FYP-10 - Connect node app to flask API

--- frontend/src/controllers/homeController.ts
+++ frontend/src/controllers/homeController.ts
@@ -4,11 +4,13 @@ import { inject, injectable } from 'inversify'
 import { TYPES } from '../types'
 import { TimingService } from '../services/timingService'
 import { ServiceManifest } from '../interfaces/serviceManifest'
+import { ApiClient } from '../services/apiClient'
 
 @injectable()
 export class HomeController implements Controller {
   public constructor(
-    @inject(TYPES.TimingService) private timingService: TimingService
+    @inject(TYPES.TimingService) private timingService: TimingService,
+    @inject(TYPES.ApiClient) private client: ApiClient
   ) { }
 
   public attachRoutes(router: Router) {
@@ -16,8 +18,9 @@ export class HomeController implements Controller {
   }
 
   private async get(req: Request, res: Response) {
+    const test: any = await this.client.route()
     const events: ServiceManifest[] = this.timingService.getEvents()
 
-    return res.render('home.html', { events: events })
+    return res.render('home.html', { events: events, test: test })
   }
 }
