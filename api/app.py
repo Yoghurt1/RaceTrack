4d17d76b4b61ae0d4d1149d36a5f3a216ffc402b
Yoghurt1 <16927289+Yoghurt1@users.noreply.github.com>
2022-05-21 11:44:36 +0100

    FYP-6 - Code review comments

--- api/app.py
+++ api/app.py
@@ -1,9 +1,8 @@
 from flask import Flask, jsonify
-
 app = Flask(__name__)
 
-@app.route('/events')
-def events():
-  return jsonify()
+@app.route('/route')
+def route():
+  return jsonify({ 'test': 'data' })
 
 app.run(host='0.0.0.0', port=3001)
